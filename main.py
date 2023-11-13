b01f7706f7f5a8a8770c166ebdf9ec20d9e5e5cd
Виктория Федорова <fiodorova.vika301296@yandex.ru>
2023-11-13 15:54:18 +0300

    Move create_statistics_table function to main.py

--- main.py
+++ main.py
@@ -2,13 +2,28 @@ import os
 
 from dotenv import load_dotenv
 from hh import count_hh_language_average_salary
-from statistics_into_table import create_statistics_table
 from superjob import count_sj_language_average_salary
+from terminaltables import AsciiTable
 
 
 LANGUAGES = ['TypeScript', 'Swift', 'Scala', 'Objective-C', 'Shell', 'Go',
              'C#', 'C', 'CSS', 'C++', 'PHP', 'Ruby', 'Python', 'Java',
              'JavaScript']
+
+
+def create_statistics_table(title, language_data):
+    table_data = [
+        ['Язык программирования', 'Вакансий найдено',
+         'Вакансий обработано', 'Средняя зарплата']
+    ]
+    for language, statistics in language_data.items():
+        row = (language, statistics['vacancies_found'],
+               statistics['vacancies_processed'],
+               statistics['average_salary'])
+        table_data.append(row)
+
+    table = AsciiTable(table_data, title)
+    return table.table
 
 
 def main():
