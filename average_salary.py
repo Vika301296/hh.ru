32e577286d573cf91b4ace50d8e138845253ede4
Виктория Федорова <fiodorova.vika301296@yandex.ru>
2023-11-16 12:30:17 +0300

    Polish average_salary function

--- average_salary.py
+++ average_salary.py
@@ -1,10 +1,10 @@
 def average_salary(from_salary=None, to_salary=None):
-    if from_salary and to_salary:
-        average_salary = int((from_salary + to_salary) / 2)
+    if not (from_salary and to_salary):
+        return None
     elif from_salary:
         average_salary = int(from_salary * 1.2)
     elif to_salary:
         average_salary = int(to_salary * 0.8)
     else:
-        return None
+        average_salary = int((from_salary + to_salary) / 2)
     return average_salary
